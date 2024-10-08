718ace34d6b9e797de53ac0fd3215f20bd448564
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2024-10-08 00:25:19 -0400

    moved clerk components to navbar component + added basic styling

---
+++ client/src/app/components/navbar.tsx
@@ -0,0 +1,47 @@
+import {
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton
+  } from '@clerk/nextjs'
+
+const Navbar: React.FC = () => {
+    return (
+        <nav className="bg-[#6c47ff] p-4 flex justify-between items-center h-24">
+            <div className="text-white text-xl font-bold">
+                <a href="/">CCNY PHD HUB</a>
+            </div>
+            <div>
+                <SignedOut>
+                    <SignUpButton>
+                        <button className="text-white px-4 py-2 rounded mr-2">
+                            Sign Up
+                        </button>
+                    </SignUpButton>
+                    <SignInButton>
+                        <button className="bg-white text-[#6c47ff] px-4 py-2 rounded">
+                            Sign In
+                        </button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                <UserButton
+                        showName
+                        appearance={{
+                            elements: {
+                                userButtonAvatarBox: 'bg-white w-12 h-12',
+                                userButtonBox: 'bg-white rounded-lg p-2',
+                                userButtonPopoverCard: 'bg-white shadow-lg rounded-lg p-4',
+                                userButtonPopoverActionButton: 'text-[#6c47ff] hover:bg-gray-100',
+                                userButtonPopoverActionButtonText: 'text-[#6c47ff]',
+                            },
+                        }}
+                    />
+                </SignedIn>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,11 +1,6 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
 import './globals.css'
+import Navbar from './components/navbar'
+import { ClerkProvider } from '@clerk/nextjs'
 export default function RootLayout({
   children,
 }: {
@@ -15,12 +10,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton showName/>
-          </SignedIn>
+          <Navbar />
           {children}
         </body>
       </html>
