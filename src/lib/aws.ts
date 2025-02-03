8dcfc5abcdfdfa3c6dbc30a2a66e3cea9885eb15
Md Wasiul Islam <99739954+mislam77@users.noreply.github.com>
2025-02-03 13:57:15 -0500

    aws fix

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -1,23 +1,37 @@
-import AWS from 'aws-sdk';
-import dotenv from 'dotenv';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
-dotenv.config({ path: '.env.local' });
+// The credentials are read from the environment automatically
+const s3Client = new S3Client({});
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+export const uploadToS3 = new PutObjectCommand({
+  Bucket: process.env.S3_BUCKET_NAME,
+  Key: 'file-name',
+  Body: 'file-body',
 });
 
-export const uploadToS3 = async (file: File) => {
-  const params = {
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: `banners/${file.name}`,
-    Body: file,
-  };
 
-  console.log('Uploading to bucket:', params.Bucket); // Add this line to debug
+// For Local Dev:
 
-  const data = await s3.upload(params).promise();
-  return data.Location;
-};
+// import AWS from 'aws-sdk';
+// import dotenv from 'dotenv';
+
+// dotenv.config({ path: '.env.local' });
+
+// const s3 = new AWS.S3({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION,
+// });
+
+// export const uploadToS3 = async (file: File) => {
+//   const params = {
+//     Bucket: process.env.AWS_S3_BUCKET_NAME,
+//     Key: `banners/${file.name}`,
+//     Body: file,
+//   };
+
+//   console.log('Uploading to bucket:', params.Bucket); // Add this line to debug
+
+//   const data = await s3.upload(params).promise();
+//   return data.Location;
+// };
